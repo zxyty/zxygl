755a32dbdd32d8f972ae2e12fc2d7fae0071b415
zxy <zxy@zxyty.com>
2018-08-29 01:23:39 +0800

    new: add camera and scene

---
+++ src/cameras/Camera.js
@@ -0,0 +1,24 @@
+import Vector3 from "../core/Vector3";
+import Matrix4 from "../core/Matrix4";
+
+export default class Camera extends Vector3 {
+  constructor(x, y, z) {
+    super(x, y, z);
+
+    this.up = new Vector3(0, 1, 0);
+    this.target = new Vector3(0, 0, 0); //lookAt center
+    this.zoom = 3;
+    this.focus = 500;
+    this.roll = 0;
+
+    this.matrix = new Matrix4();
+  }
+
+  updateMatrix() {
+    this.matrix.lookAt(this, this.target, this.up);
+  }
+
+  toString() {
+    return "Camera ( " + this.x + ", " + this.y + ", " + this.z + " )";
+  }
+}
---
+++ src/scenes/Scene.js
@@ -0,0 +1,20 @@
+export default class Scene {
+  constructor() {
+    this.objects = new Array();
+  }
+
+  add(object) {
+    this.objects.push(object);
+  }
+  remove(object) {
+    for (let i = 0; i < this.objects.length; i++) {
+      const element = this.objects[i];
+      if (object == element) {
+        alert("scene-remove");
+      }
+    }
+  }
+  toString() {
+    return "Scene ( " + this.objects + " )";
+  }
+}
