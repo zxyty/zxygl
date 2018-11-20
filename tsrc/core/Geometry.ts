290e594f673f374441ca0e160f0cc15d73495e9d
zxy <zxy@zxyty.com>
2018-11-20 15:55:30 +0800

    new: Scene添加addObject和removeObject方法

--- tsrc/core/Geometry.ts
+++ tsrc/core/Geometry.ts
@@ -20,41 +20,5 @@ export default class Geometry {
     
     this.faces.push(new Face4(a, b, c, d, null, null))
   }
-  computeNormals() {
-    let v, f, vA, vB, vC, cb, ab, normal;
-    
-    for (v = 0; v < this.vertices.length; v++) {
-      this.vertices[v].normal.set(0, 0 ,0);
-    }
-
-    for(f = 0; f < this.faces.length; f++) {
-      vA = this.vertices[this.faces[f].a];
-      vB = this.vertices[this.faces[f].b];
-      vC = this.vertices[this.faces[f].c];
-
-      cb = new Vector3();
-      ab = new Vector3();
-      normal = new Vector3();
-
-      cb.sub(vC.position, vB.position);
-      ab.sub(vA.position, vB.position);
-      cb.crossSelf(ab);
-
-      if(!cb.isZero()) {
-        cb.normalize();
-      }
-
-      this.faces[f].normal = cb;
-
-      vA.normal.addSelf(normal);
-      vB.normal.addSelf(normal);
-      vC.normal.addSelf(normal);
-
-      if(this.faces[f] instanceof Face4) {
-        let face =  this.faces[f] as Face4;
-        this.vertices[face.d].normal.addSelf(normal);
-      }
-
-    }
-  }
+  
 }
--- tsrc/scenes/Scene.ts
+++ tsrc/scenes/Scene.ts
@@ -8,14 +8,23 @@ export default class Scene {
   }
 
   add(object: Object3D) {
+    this.addObject(object);
+  }
+
+  addObject(object: Object3D) {
     this.objects.push(object);
   }
   
   remove(object: Object3D) {
+    this.removeObject(object);
+  }
+
+  removeObject(object: Object3D) {
     for (let i = 0; i < this.objects.length; i++) {
       const element = this.objects[i];
-      if (object == element) {
-        alert("scene-remove");
+      if(object == element) {
+        this.objects.splice(i, 1);
+        return;
       }
     }
   }
